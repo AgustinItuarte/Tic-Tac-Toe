7d80fa57f1bae61b4042904305923ca7cfe7a4da
AgustinItuarte <agustin242536@gmail.com>
2022-12-25 15:06:33 -0300

    Funcion para que el jugador coloque su jugada

--- scripts.js
+++ scripts.js
@@ -19,15 +19,34 @@ const jugador = (nombre, numero, tipoJugada) => {
 }
 
 let juego = (function() {
-    let jugadas = tablaJuego.pLista;
+
+    const jugadas = tablaJuego.pLista;
 
     const jugando = (jugador1, jugador2) => {
-        if (condition) {
-            
+
+        for (let i = 0; i < jugadas.length; i++) {
+
+            jugadas[i].addEventListener('click', () => {
+
+                mostrarTabla.cambiarCasilla(i, jugador1.tipoJugada);
+
+            })
+
         }
+
+        /* jugadas.forEach(p => {
+
+            p.addEventListener('click', () => {
+                p.innerHTML = jugador1.tipoJugada;
+                console.log(p)
+        })
+
+        }); */
+
     }
 
     return { jugando };
+
 })();
 
 const btnIngreso = document.querySelector('.btn-ingreso');
@@ -49,11 +68,14 @@ btnIngreso.addEventListener('click', () => {
 
 btnJugar.addEventListener('click', function(e) {
     const form = document.querySelector('.form-jugadores')
-    let jugador1 = form.elements[0].value;
-    let jugador2 = form.elements[1].value;
-    
+    let nomJugador1 = form.elements[0].value;
+    let nomJugador2 = form.elements[1].value;
+
     e.preventDefault();
     cerrarIngreso();
-})
 
-mostrarTabla.cambiarCasilla(8, 'x');
+    const jugador1 = jugador(nomJugador1, 1, 'X')
+    const jugador2 = jugador(nomJugador2, 2, 'O')
+
+    juego.jugando(jugador1, jugador2);
+})
